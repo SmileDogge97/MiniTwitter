e3ab23c8141db9f6bba828cdd0770a197f7b59c6
Jose Francisco Jarquin Martinez <jose.jarquin@neoris.com>
2022-07-08 17:40:11 -0500

    ya funciona el registro de usuarios

--- app/src/main/java/com/example/minitwitter/ui/SignUpActivity.java
+++ app/src/main/java/com/example/minitwitter/ui/SignUpActivity.java
@@ -2,18 +2,33 @@ package com.example.minitwitter.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.minitwitter.R;
+import com.example.minitwitter.retrofit.MiniTwitterClient;
+import com.example.minitwitter.retrofit.MiniTwitterService;
+import com.example.minitwitter.retrofit.request.RequestSignUp;
+import com.example.minitwitter.retrofit.response.ResponseAuth;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnSignUp;
     TextView tvGoLogin;
+    EditText etUserName, etEmail, etPassword;
+    MiniTwitterClient miniTwitterClient;
+    MiniTwitterService miniTwitterService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +37,76 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         getSupportActionBar().hide();
 
-        btnSignUp = findViewById(R.id.buttonSignUp);
-        tvGoLogin = findViewById(R.id.textViewGoLogin);
+        retrofitinit();
+        findViews();
+        events();
+    }
 
+    private void retrofitinit() {
+        miniTwitterClient = MiniTwitterClient.getInstance();
+        miniTwitterService = miniTwitterClient.getMiniTwitterService();
+    }
+
+    private void events() {
         btnSignUp.setOnClickListener(this);
         tvGoLogin.setOnClickListener(this);
     }
 
+    private void findViews() {
+        btnSignUp = findViewById(R.id.buttonSignUp);
+        tvGoLogin = findViewById(R.id.textViewGoLogin);
+        etUserName = findViewById(R.id.editTextUsername);
+        etEmail = findViewById(R.id.editTextEmail);
+        etPassword = findViewById(R.id.editTextPassword);
+    }
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         int id = view.getId();
         
         switch (id) {
             case R.id.buttonSignUp:
+                goToSignUp();
                 break;
             case R.id.textViewGoLogin:
                 goToLogin();
                 break;
+        }
+    }
+
+    private void goToSignUp() {
+        String username = etUserName.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (username.isEmpty()){
+            etUserName.setError("El nombre de usuario es requerido");
+        } else if (username.isEmpty()){
+            etEmail.setError("El email es requerido");
+        } else if(password.isEmpty() || password.length()<4){
+            etPassword.setError("La contraseña es requerida y debe tener al menos 4 caracteres");
+        } else {
+            String code = "UDEMYANDROID";
+            RequestSignUp requestSignUp = new RequestSignUp(username, email, password, code);
+            Call<ResponseAuth> call = miniTwitterService.doSignUp(requestSignUp);
+            call.enqueue(new Callback<ResponseAuth>() {
+                @Override
+                public void onResponse(Call<ResponseAuth> call, Response<ResponseAuth> response) {
+                    if (response.isSuccessful()){
+                        Intent i = new Intent(SignUpActivity.this, DashboardActivity.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        Toast.makeText(SignUpActivity.this, "Algo ha ido mal, revise los datos de registro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseAuth> call, Throwable t) {
+                    Toast.makeText(SignUpActivity.this, "Error en la conexión. Intentelo de nuevo.", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
